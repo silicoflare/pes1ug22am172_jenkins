c52699ccd456311168101bed4c47affe6858056a
Suraj B M <silicoflare@gmail.com>
2025-03-10 15:24:41 +0530

    init: initial commit

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "SRN: PES1UG22AM172\n";
+  return 0;
+}
