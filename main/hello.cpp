ba2b7d308d2cc9dc76f82ebc10adf31779818b73
Suraj B M <silicoflare@gmail.com>
2025-03-10 16:12:12 +0530

    feat: add webhook test line

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,6 +8,6 @@ int main(void)
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "SRN: PES1UG22AM172\n";
-  cout << "Making a change to see if webhook works.\n";
+  cout << "Making a change to see if webhook works\n";
   return 0;
 }
